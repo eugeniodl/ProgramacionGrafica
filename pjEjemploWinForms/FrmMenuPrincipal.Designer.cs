99aca38b4bf098fee4b1ec64ccee0611199efd14
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-11-10 16:09:28 -0600

    pjEjemploWinForms

--- pjEjemploWinForms/FrmMenuPrincipal.Designer.cs
+++ pjEjemploWinForms/FrmMenuPrincipal.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace pjEjemploWinForms
 {
-    partial class FrmMenuPrincipal
+    partial class frmMenuPrincipal
     {
         /// <summary>
         /// Required designer variable.
@@ -28,108 +28,214 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMenuPrincipal));
-            pnlContenedorPrincipal = new Panel();
-            panel1 = new Panel();
-            pnlMenu = new Panel();
-            pnlBarraTitulo = new Panel();
-            pnlContenedorForm = new Panel();
-            btnMembresia = new Button();
-            pnlContenedorPrincipal.SuspendLayout();
-            pnlMenu.SuspendLayout();
-            SuspendLayout();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMenuPrincipal));
+            this.pnlContenedorPrincipal = new System.Windows.Forms.Panel();
+            this.pnlContenedorForm = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlMenu = new System.Windows.Forms.Panel();
+            this.btnMenu = new System.Windows.Forms.Button();
+            this.btnMembresia = new System.Windows.Forms.Button();
+            this.pnlBarraTitulo = new System.Windows.Forms.Panel();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.btnMaximizar = new System.Windows.Forms.Button();
+            this.btnMinimizar = new System.Windows.Forms.Button();
+            this.btnNormal = new System.Windows.Forms.Button();
+            this.tmExpandirMenu = new System.Windows.Forms.Timer(this.components);
+            this.tmContraerMenu = new System.Windows.Forms.Timer(this.components);
+            this.pnlContenedorPrincipal.SuspendLayout();
+            this.pnlMenu.SuspendLayout();
+            this.pnlBarraTitulo.SuspendLayout();
+            this.SuspendLayout();
             // 
             // pnlContenedorPrincipal
             // 
-            pnlContenedorPrincipal.BackColor = Color.FromArgb(64, 69, 76);
-            pnlContenedorPrincipal.Controls.Add(pnlContenedorForm);
-            pnlContenedorPrincipal.Controls.Add(panel1);
-            pnlContenedorPrincipal.Controls.Add(pnlMenu);
-            pnlContenedorPrincipal.Controls.Add(pnlBarraTitulo);
-            pnlContenedorPrincipal.Dock = DockStyle.Fill;
-            pnlContenedorPrincipal.Location = new Point(0, 0);
-            pnlContenedorPrincipal.Name = "pnlContenedorPrincipal";
-            pnlContenedorPrincipal.Size = new Size(1451, 699);
-            pnlContenedorPrincipal.TabIndex = 0;
-            // 
-            // panel1
-            // 
-            panel1.BackColor = Color.FromArgb(55, 61, 69);
-            panel1.Dock = DockStyle.Bottom;
-            panel1.Location = new Point(307, 599);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(1144, 100);
-            panel1.TabIndex = 2;
-            // 
-            // pnlMenu
-            // 
-            pnlMenu.BackColor = Color.FromArgb(29, 34, 39);
-            pnlMenu.Controls.Add(btnMembresia);
-            pnlMenu.Dock = DockStyle.Left;
-            pnlMenu.Location = new Point(0, 53);
-            pnlMenu.Name = "pnlMenu";
-            pnlMenu.Size = new Size(307, 646);
-            pnlMenu.TabIndex = 1;
-            // 
-            // pnlBarraTitulo
-            // 
-            pnlBarraTitulo.BackColor = Color.FromArgb(214, 61, 92);
-            pnlBarraTitulo.Dock = DockStyle.Top;
-            pnlBarraTitulo.Location = new Point(0, 0);
-            pnlBarraTitulo.Name = "pnlBarraTitulo";
-            pnlBarraTitulo.Size = new Size(1451, 53);
-            pnlBarraTitulo.TabIndex = 0;
+            this.pnlContenedorPrincipal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(69)))), ((int)(((byte)(76)))));
+            this.pnlContenedorPrincipal.Controls.Add(this.pnlContenedorForm);
+            this.pnlContenedorPrincipal.Controls.Add(this.panel1);
+            this.pnlContenedorPrincipal.Controls.Add(this.pnlMenu);
+            this.pnlContenedorPrincipal.Controls.Add(this.pnlBarraTitulo);
+            this.pnlContenedorPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlContenedorPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.pnlContenedorPrincipal.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlContenedorPrincipal.Name = "pnlContenedorPrincipal";
+            this.pnlContenedorPrincipal.Size = new System.Drawing.Size(973, 517);
+            this.pnlContenedorPrincipal.TabIndex = 0;
             // 
             // pnlContenedorForm
             // 
-            pnlContenedorForm.Dock = DockStyle.Fill;
-            pnlContenedorForm.Location = new Point(307, 53);
-            pnlContenedorForm.Name = "pnlContenedorForm";
-            pnlContenedorForm.Size = new Size(1144, 546);
-            pnlContenedorForm.TabIndex = 3;
+            this.pnlContenedorForm.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlContenedorForm.Location = new System.Drawing.Point(230, 55);
+            this.pnlContenedorForm.Name = "pnlContenedorForm";
+            this.pnlContenedorForm.Size = new System.Drawing.Size(743, 368);
+            this.pnlContenedorForm.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(61)))), ((int)(((byte)(69)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(230, 423);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(743, 94);
+            this.panel1.TabIndex = 2;
+            // 
+            // pnlMenu
+            // 
+            this.pnlMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(34)))), ((int)(((byte)(39)))));
+            this.pnlMenu.Controls.Add(this.btnMenu);
+            this.pnlMenu.Controls.Add(this.btnMembresia);
+            this.pnlMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlMenu.Location = new System.Drawing.Point(0, 55);
+            this.pnlMenu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlMenu.Name = "pnlMenu";
+            this.pnlMenu.Size = new System.Drawing.Size(230, 462);
+            this.pnlMenu.TabIndex = 1;
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMenu.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMenu.FlatAppearance.BorderSize = 0;
+            this.btnMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMenu.Image = ((System.Drawing.Image)(resources.GetObject("btnMenu.Image")));
+            this.btnMenu.Location = new System.Drawing.Point(166, 3);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(64, 56);
+            this.btnMenu.TabIndex = 1;
+            this.btnMenu.UseVisualStyleBackColor = true;
+            this.btnMenu.Click += new System.EventHandler(this.btnMenu_Click);
             // 
             // btnMembresia
             // 
-            btnMembresia.Cursor = Cursors.Hand;
-            btnMembresia.FlatAppearance.BorderSize = 0;
-            btnMembresia.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 69, 76);
-            btnMembresia.FlatAppearance.MouseOverBackColor = Color.FromArgb(38, 45, 53);
-            btnMembresia.FlatStyle = FlatStyle.Flat;
-            btnMembresia.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            btnMembresia.ForeColor = Color.Silver;
-            btnMembresia.Image = (Image)resources.GetObject("btnMembresia.Image");
-            btnMembresia.ImageAlign = ContentAlignment.MiddleLeft;
-            btnMembresia.Location = new Point(0, 62);
-            btnMembresia.Name = "btnMembresia";
-            btnMembresia.Size = new Size(304, 44);
-            btnMembresia.TabIndex = 0;
-            btnMembresia.Text = "Membresía";
-            btnMembresia.TextAlign = ContentAlignment.MiddleLeft;
-            btnMembresia.TextImageRelation = TextImageRelation.ImageBeforeText;
-            btnMembresia.UseVisualStyleBackColor = true;
+            this.btnMembresia.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMembresia.FlatAppearance.BorderSize = 0;
+            this.btnMembresia.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(69)))), ((int)(((byte)(76)))));
+            this.btnMembresia.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(45)))), ((int)(((byte)(53)))));
+            this.btnMembresia.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMembresia.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnMembresia.ForeColor = System.Drawing.Color.Silver;
+            this.btnMembresia.Image = ((System.Drawing.Image)(resources.GetObject("btnMembresia.Image")));
+            this.btnMembresia.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMembresia.Location = new System.Drawing.Point(0, 83);
+            this.btnMembresia.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnMembresia.Name = "btnMembresia";
+            this.btnMembresia.Size = new System.Drawing.Size(230, 46);
+            this.btnMembresia.TabIndex = 0;
+            this.btnMembresia.Text = "Membresía";
+            this.btnMembresia.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMembresia.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnMembresia.UseVisualStyleBackColor = true;
+            this.btnMembresia.Click += new System.EventHandler(this.btnMembresia_Click);
             // 
-            // FrmMenuPrincipal
+            // pnlBarraTitulo
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1451, 699);
-            Controls.Add(pnlContenedorPrincipal);
-            FormBorderStyle = FormBorderStyle.None;
-            Name = "FrmMenuPrincipal";
-            Opacity = 0.95D;
-            Text = "FrmMenuPrincipal";
-            pnlContenedorPrincipal.ResumeLayout(false);
-            pnlMenu.ResumeLayout(false);
-            ResumeLayout(false);
+            this.pnlBarraTitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(214)))), ((int)(((byte)(61)))), ((int)(((byte)(92)))));
+            this.pnlBarraTitulo.Controls.Add(this.btnMaximizar);
+            this.pnlBarraTitulo.Controls.Add(this.btnCerrar);
+            this.pnlBarraTitulo.Controls.Add(this.btnMinimizar);
+            this.pnlBarraTitulo.Controls.Add(this.btnNormal);
+            this.pnlBarraTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlBarraTitulo.Location = new System.Drawing.Point(0, 0);
+            this.pnlBarraTitulo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlBarraTitulo.Name = "pnlBarraTitulo";
+            this.pnlBarraTitulo.Size = new System.Drawing.Size(973, 55);
+            this.pnlBarraTitulo.TabIndex = 0;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCerrar.FlatAppearance.BorderSize = 0;
+            this.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCerrar.Image = ((System.Drawing.Image)(resources.GetObject("btnCerrar.Image")));
+            this.btnCerrar.Location = new System.Drawing.Point(916, 0);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(57, 53);
+            this.btnCerrar.TabIndex = 2;
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // btnMaximizar
+            // 
+            this.btnMaximizar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMaximizar.FlatAppearance.BorderSize = 0;
+            this.btnMaximizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMaximizar.Image = ((System.Drawing.Image)(resources.GetObject("btnMaximizar.Image")));
+            this.btnMaximizar.Location = new System.Drawing.Point(853, 0);
+            this.btnMaximizar.Name = "btnMaximizar";
+            this.btnMaximizar.Size = new System.Drawing.Size(57, 53);
+            this.btnMaximizar.TabIndex = 1;
+            this.btnMaximizar.UseVisualStyleBackColor = true;
+            this.btnMaximizar.Click += new System.EventHandler(this.btnMaximizar_Click);
+            // 
+            // btnMinimizar
+            // 
+            this.btnMinimizar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMinimizar.FlatAppearance.BorderSize = 0;
+            this.btnMinimizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimizar.Image = ((System.Drawing.Image)(resources.GetObject("btnMinimizar.Image")));
+            this.btnMinimizar.Location = new System.Drawing.Point(790, 0);
+            this.btnMinimizar.Name = "btnMinimizar";
+            this.btnMinimizar.Size = new System.Drawing.Size(57, 53);
+            this.btnMinimizar.TabIndex = 0;
+            this.btnMinimizar.UseVisualStyleBackColor = true;
+            this.btnMinimizar.Click += new System.EventHandler(this.btnMinimizar_Click);
+            // 
+            // btnNormal
+            // 
+            this.btnNormal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNormal.FlatAppearance.BorderSize = 0;
+            this.btnNormal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNormal.Image = ((System.Drawing.Image)(resources.GetObject("btnNormal.Image")));
+            this.btnNormal.Location = new System.Drawing.Point(853, 0);
+            this.btnNormal.Name = "btnNormal";
+            this.btnNormal.Size = new System.Drawing.Size(57, 53);
+            this.btnNormal.TabIndex = 3;
+            this.btnNormal.UseVisualStyleBackColor = true;
+            this.btnNormal.Click += new System.EventHandler(this.btnNormal_Click);
+            // 
+            // tmExpandirMenu
+            // 
+            this.tmExpandirMenu.Interval = 15;
+            this.tmExpandirMenu.Tick += new System.EventHandler(this.tmExpandirMenu_Tick);
+            // 
+            // tmContraerMenu
+            // 
+            this.tmContraerMenu.Interval = 15;
+            this.tmContraerMenu.Tick += new System.EventHandler(this.tmContraerMenu_Tick);
+            // 
+            // frmMenuPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(973, 517);
+            this.Controls.Add(this.pnlContenedorPrincipal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "frmMenuPrincipal";
+            this.Opacity = 0.95D;
+            this.Text = "frmMenuPrincipal";
+            this.pnlContenedorPrincipal.ResumeLayout(false);
+            this.pnlMenu.ResumeLayout(false);
+            this.pnlBarraTitulo.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
 
         private Panel pnlContenedorPrincipal;
         private Panel pnlBarraTitulo;
-        private Panel pnlMenu;
         private Panel panel1;
-        private Panel pnlContenedorForm;
+        private Panel pnlMenu;
         private Button btnMembresia;
+        private Panel pnlContenedorForm;
+        private Button btnMenu;
+        private System.Windows.Forms.Timer tmExpandirMenu;
+        private System.Windows.Forms.Timer tmContraerMenu;
+        private Button btnCerrar;
+        private Button btnMaximizar;
+        private Button btnMinimizar;
+        private Button btnNormal;
     }
 }
